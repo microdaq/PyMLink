45165f1112c6481635fdb4c721e47a3c91a757a9
witczenko <witczenko@gmail.com>
2020-11-11 18:57:25 +0100

    Add simplified Device for mocking purpose

--- tests/mock_device.py
+++ tests/mock_device.py
@@ -2,31 +2,19 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # Embedded-solutions 2020, www.microdaq.org
 
+import ctypes
+import microdaq
 
-class MLinkError(Exception):
-    pass
 
-
-class Device:
+class Device(microdaq.Device):
     def __init__(self, ip):
-        self._serie = 2000
-        self._adc = 9
-        self._dac = 6
-        self._cpu = 1
-        self._mem = 2
+        self._linkfd = ctypes.c_int32()
+        self._ip = "0.0.0.0"
+        self._connectionless = False
+        self._ao_scan_ch = 0
+        self._mdaq_hwid = 0
+        self._ai_scan_channels = None
+        self._mdaq_hwid = (0, 0, 0, 0, 0)
 
-        if ip == "999.999.999.999":
-            raise MLinkError 
-        _ = ip
-
-    def ai_read(self, channels, *args):
-        return [0.0]*len(channels)
-
-    def disconnect(self):
-        return True
-
-    def get_str_hw_info(self):
-        return f'MicroDAQ E{self._serie}-ADC0{self._adc}-DAC0{self._dac}-{self._cpu}{self._mem}'
-
-    def get_hw_info(self):
-        return (self._serie, self._adc, self._dac, self._cpu, self._mem)
+    def _raise_exception(self, res):
+        ...
