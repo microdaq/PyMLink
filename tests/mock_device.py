10ac2dc264f03a0c3e8b5ff6f4b54f09edd66154
witczenko <witczenko@gmail.com>
2020-11-12 18:03:35 +0100

    Add doc strings to mock_device module

--- tests/mock_device.py
+++ tests/mock_device.py
@@ -2,12 +2,26 @@
 # file 'LICENSE.txt', which is part of this source code package.
 # Embedded-solutions 2020, www.microdaq.org
 
+"""Mocked class for microdaq.Device"""
+
 import ctypes
 import microdaq
 
 
 class Device(microdaq.Device):
-    def __init__(self, ip):
+    """Mocked implementation of the microdaq.Device class. It suppresses only
+    MLinkError exceptions in order to validate Python wrapper only.
+
+    The assumption is, whenever MLinkError exception appear that means we passed
+    through API without any major problems and we properly called function from
+    MLink sharded library.
+
+    It doesn't require connected MicroDAQ device.
+    """
+
+    def __init__(self, ip, maintain_connection=False):
+        _ = ip
+        self._connectionless = maintain_connection
         self._linkfd = ctypes.c_int32()
         self._ip = "0.0.0.0"
         self._connectionless = False
@@ -17,4 +31,4 @@ class Device(microdaq.Device):
         self._mdaq_hwid = (0, 0, 0, 0, 0)
 
     def _raise_exception(self, res):
-        ...
+        pass
